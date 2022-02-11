1a467165969371bb0c7dad92826f53c30a11db51
Jun Kong <kongjun18@outlook.com>
2022-02-11 17:43:00 +0800

    change(editor): remove unused tex nvim-autopairs rules

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -111,26 +111,6 @@ function config.autopairs()
       end)
       :use_key("]"),
   })
-
-  autopairs.add_rule(Rule("$$", "$$", "tex"))
-  autopairs.add_rules({
-    Rule("$", "$", { "tex", "latex" }) -- don't add a pair if the next character is %
-      :with_pair(cond.not_after_regex_check("%%")) -- don't add a pair if  the previous character is xxx
-      :with_pair(cond.not_before_regex_check("xxx", 3)) -- don't move right when repeat character
-      :with_move(cond.none()) -- don't delete if the next character is xx
-      :with_del(cond.not_after_regex_check("xx")) -- disable  add newline when press <cr>
-      :with_cr(cond.none()),
-  })
-
-  autopairs.add_rules({
-    Rule("$$", "$$", "tex"):with_pair(function(opts)
-      print(vim.inspect(opts))
-      if opts.line == "aa $$" then
-        -- don't add pair on that line
-        return false
-      end
-    end),
-  })
 end
 
 function config.treesitter()
