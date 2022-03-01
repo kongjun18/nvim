9902bf8a0f769784a7a508e66edab410d08c2841
Jun Kong <kongjun18@outlook.com>
2022-03-01 09:58:22 +0800

    feat(lsp): configure null-ls linters

--- lua/modules/lsp/providers/linters.lua
+++ lua/modules/lsp/providers/linters.lua
@@ -1,4 +1,26 @@
 local M = {}
-M.linters = {}
+-- NOTE: null-ls is disable in C/C++ buffer
+M.linters = {
+  sh = "shellcheck",
+  cpp = "cppcheck",
+  lua = "luacheck",
+  javascript = "eslint",
+  docker = "hadolint",
+}
 M.opts = {}
+
+-----------------------------------------------------------------------
+-- Use hadolint in docker
+-- Bash script(/usr/local/bin/hadolint):
+--
+-- #!/bin/bash
+-- dockerfile="$1"
+-- shift
+-- docker run --rm -i hadolint/hadolint hadolint "$@" - < "$dockerfile"
+-----------------------------------------------------------------------
+-- I don't know why this configuration works, but it works.
+M.opts.hadolint = {
+  args = { "$FILENAME", "--no-fail", "--format=json" },
+}
+
 return M
