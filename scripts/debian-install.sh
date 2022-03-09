a17784b04dbc838c499223bd1f94264232fc8067
Kong Jun <kongjun18@outlook.com>
2022-03-09 15:34:22 +0800

    build: only install C/C++ and neovim dependencies

--- scripts/debian-install.sh
+++ scripts/debian-install.sh
@@ -3,29 +3,21 @@
 # Install neovim dependencies.
 # Support Debian bullseys or later.
 
-# Neovim plugins dependencies
-apt install -y curl wget
-apt install -y git gcc make python3 pip nodejs npm
-pip3 install pynvim
-# Tag system
-apt install -y global universal-ctags
-pip3 install pygments
-# C++ development
-apt install -y ccls cmake gdb clang-format clang-tidy bear doxygen graphviz
+apt-get update \
+&& apt-get install -y ca-certificates apt-transport-https openssh-client \
+    curl wget git unzip build-essential 
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+apt-get update \
+&& apt-get install -y \
+    python3 python3-pip python3-venv \
+    nodejs \
+    ripgrep fd-find xclip \
+    global universal-ctags \
+    cmake gdb clang-format clang-tidy bear doxygen graphviz cppcheck ccls \
+    luarocks \
+    shellcheck \
+&& rm -rf /var/lib/apt/lists/*
+pip3 install pygments \
+    pynvim neovim neovim-remote
+luarocks install luacheck
 mkdir -p ~/.local/share/nvim/lsp_servers/ccls
-# Lua development
-apt install -y unzip luarocks
-# Github mirrors:
-#     - https://hub.fastgit.org
-#     - https://github.cnpmjs.org
-wget https://hub.fastgit.org/JohnnyMorganz/StyLua/releases/download/v0.11.2/stylua-0.11.2-linux.zip -O /tmp/stylua.zip \
-    && unzip -o /tmp/stylua.zip -d /usr/local/bin \
-    && chmod a+x /usr/local/bin/stylua
-luarocks install ldoc luacheck
-# Javascript development
-npm install -g eslint standard jsdoc
-# Bash shell
-apt install -y shellcheck
-# System utilites
-apt install -y ripgrep fd-find xclip
-apt autoremove -y
