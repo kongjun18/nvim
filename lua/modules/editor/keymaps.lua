d567ae233c016f812c8ba951cf305601b8f09f40
Kong Jun <kongjun18@outlook.com>
2022-03-11 11:36:26 +0800

    refactor: substitute packer.nvim's key with nvim's CmdlineEnter event

--- lua/modules/editor/keymaps.lua
+++ lua/modules/editor/keymaps.lua
@@ -11,15 +11,41 @@ local M = {
   },
   ["<Leader>f"] = {
     ["name"] = "+Fuzzy Find",
-    ["f"] = { "<Cmd>Telescope find_files<CR>", "Find Files" },
-    ["g"] = { "<Cmd>Telescope live_grep<CR>", "Live Grep" },
-    ["b"] = { "<Cmd>Telescope buffers<CR>", "Find Buffers" },
-    ["h"] = { "<Cmd>Telescope help_tags<CR>", "Find Help" },
-    ["d"] = { "<Cmd>Telescope tags<CR>", "Find Tags(definition)" },
-    ["m"] = { "<Cmd>Telescope man_pages<CR>", "Find Man Pages" },
-    ["p"] = { "<Cmd>Telescope projects<CR>", "Find projects" },
+    ["f"] = {
+      "<Cmd>lua TelescopeBuiltin('find_files')<CR>",
+      "Find Files",
+    },
+    ["g"] = {
+      "<Cmd>lua TelescopeBuiltin('live_grep')<CR>",
+      "Live Grep",
+    },
+    ["b"] = {
+      "<Cmd>lua TelescopeBuiltin('buffers')<CR>",
+      "Find Buffers",
+    },
+    ["h"] = {
+      "<Cmd>lua TelescopeBuiltin('help_tags')<CR>",
+      "Find Help",
+    },
+    ["d"] = {
+      "<Cmd>lua TelescopeBuiltin('tags')<CR>",
+      "Find Tags(definition)",
+    },
+    ["m"] = {
+      "<Cmd>lua TelescopeBuiltin('man_pages')<CR>",
+      "Find Man Pages",
+    },
+    ["p"] = {
+      function()
+        vim.cmd([[PackerLoad telescope.nvim]])
+        vim.cmd([[Telescope projects]])
+      end,
+      "Find projects",
+    },
     ["a"] = {
-      "<Cmd>lua require('telescope').extensions.asynctasks.all()<CR>",
+      function()
+        require("telescope").extensions.asynctasks.all()
+      end,
       "Find AsyncTask",
     },
     ["t"] = { "<Cmd>TodoTelescope<CR>", "Find Todo Comments" },
@@ -27,7 +53,7 @@ local M = {
   -- Terminal
   ["<M-=>"] = {
     function()
-      vim.cmd([[silent! PackerLoad vim-terminal-help]])
+      vim.cmd([[PackerLoad vim-terminal-help]])
       vim.fn.TerminalToggle()
     end,
     "Toggle Terminal",
@@ -74,5 +100,10 @@ vim.keymap.set({ "i", "s" }, t("<C-k>"), function()
     snip.jump(-1)
   end
 end)
+
+function TelescopeBuiltin(cmd)
+  vim.cmd([[PackerLoad telescope.nvim]])
+  require("telescope.builtin")[cmd]()
+end
 
 return M
