c9c71d9fae581f676cca4ce7db08f21637e9c510
Kong Jun <kongjun18@outlook.com>
2022-04-29 15:30:31 +0800

    fix(lsp): lsp window/showMessage handler prints error

--- lua/modules/lsp/config.lua
+++ lua/modules/lsp/config.lua
@@ -145,22 +145,24 @@ function config.custom_ui()
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
   end
 
-  -- Use nvim-notify to display LSP messages
-  vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
-    local client = vim.lsp.get_client_by_id(ctx.client_id)
-    local lvl = ({
-      "ERROR",
-      "WARN",
-      "INFO",
-      "DEBUG",
-    })[result.type]
-    notify({ result.message }, lvl, {
-      title = "LSP | " .. client.name,
-      timeout = 10000,
-      keep = function()
-        return lvl == "ERROR" or lvl == "WARN"
-      end,
-    })
+  local ok, notify = require("notify")
+  if ok then
+    vim.lsp.handlers["window/showMessage"] = function(_, result, ctx)
+      local client = vim.lsp.get_client_by_id(ctx.client_id)
+      local lvl = ({
+        "ERROR",
+        "WARN",
+        "INFO",
+        "DEBUG",
+      })[result.type]
+      notify({ result.message }, lvl, {
+        title = "LSP | " .. client.name,
+        timeout = 10000,
+        keep = function()
+          return lvl == "ERROR" or lvl == "WARN"
+        end,
+      })
+    end
   end
 end
 
