cae808d2223843dfd3064bfa292eb7e784e21986
Jun Kong <kongjun18@outlook.com>
2022-05-01 20:16:04 +0800

    change(lsp): linter plugin null-ls.nvim ==> nvim-lint

--- lua/modules/lsp/providers/linters.lua
+++ lua/modules/lsp/providers/linters.lua
@@ -1,26 +1,13 @@
 local M = {}
--- NOTE: null-ls is disable in C/C++ buffer
+
 M.linters = {
-  sh = "shellcheck",
-  cpp = "cppcheck",
-  lua = "luacheck",
-  javascript = "eslint",
-  docker = "hadolint",
+  sh = { "shellcheck" },
+  go = { "golangcilint" },
+  cpp = { "cppcheck" },
+  lua = { "luacheck" },
+  javascript = { "eslint" },
+  dockerfile = { "hadolint" },
 }
 M.opts = {}
-
------------------------------------------------------------------------
--- Use hadolint in docker
--- Bash script(/usr/local/bin/hadolint):
---
--- #!/bin/bash
--- dockerfile="$1"
--- shift
--- docker run --rm -i hadolint/hadolint hadolint "$@" - < "$dockerfile"
------------------------------------------------------------------------
--- I don't know why this configuration works, but it works.
-M.opts.hadolint = {
-  args = { "$FILENAME", "--no-fail", "--format=json" },
-}
 
 return M
