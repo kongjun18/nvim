5e5c19019b8893a336f043abeddb72de9c3af1e7
Kong Jun <kongjun18@outlook.com>
2022-05-03 18:05:10 +0800

    feat(dap): finish dap module

---
+++ lua/modules/dap/keymaps.lua
@@ -0,0 +1,32 @@
+local M = {
+  ["<F6>"] = {
+    ":lua require'dap'.toggle_breakpoint()<CR>",
+    "Toggle Breakpoint",
+  },
+  ["<Leader><F6>"] = {
+    ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+    "Toggle Conditional Breakpoint",
+  },
+  ["<F3>"] = { ":lua require'dap'.continue()<CR>", "Continue" },
+  ["<F9>>"] = { ":lua require'dap'.step_into()<CR>", "Step Into" },
+  ["<F10>"] = { ":lua require'dap'.step_over()<CR>", "Step Over" },
+  ["<F8>"] = { ":lua require'dap'.step_out()<CR>", "Step Out" },
+  ["<C-S-F5>"] = { ":lua require'dap'.run_last()<CR>", "Restart Debug" },
+  ["<F1>"] = {
+    function()
+      require("dap").close()
+      require("dap.repl").close()
+      require("dapui").close()
+      vim.cmd("DapVirtualTextForceRefresh")
+    end,
+    "Exit Debug",
+  },
+}
+-- Evaluate expression
+vim.keymap.set(
+  { "n", "v" },
+  "<M-k>",
+  ":lua require('dapui').eval()<CR>",
+  { silent = true }
+)
+return M
