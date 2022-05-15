3225da5b3b1939c3ab6a93f3034d5afb02698feb
Kong Jun <kongjun18@outlook.com>
2022-05-15 18:19:57 +0800

    build: add ubuntu-install.sh

---
+++ scripts/ubuntu-install.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+#
+# Install neovim dependencies.
+# Support Ubuntu 18.04 or later.
+
+apt-get update \
+&& apt-get install -y ca-certificates apt-transport-https openssh-client \
+    curl wget git unzip build-essential
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+apt-get update \
+&& apt-get install -y \
+    python3 python3-pip python3-venv \
+    nodejs \
+    xclip \
+    global \
+    cmake gdb clang-format clang-tidy bear doxygen graphviz cppcheck \
+    lua-check \
+    shellcheck \
+&& rm -rf /var/lib/apt/lists/* \
+&& apt-get update
+pip3 install pygments \
+    pynvim neovim neovim-remote
+if ! type snap &> /dev/null; then
+  echo -e "\e[1;31msnap not found!\e[0m" > /dev/stderr
+  exit 1
+fi
+snap install --classic ccls
+snap install --classic ripgrep
+snap install --classic universal-ctags
+mkdir -p ~/.local/share/nvim/lsp_servers/ccls
