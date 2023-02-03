27f5a8a7e9d1b2af07161f090294b43c5f33bd44
Jun Kong <kongjun18@outlook.com>
2023-02-03 19:09:43 +0800

    feat(ui): separated gitsign and LSP diagnostic

--- lua/modules/ui/init.lua
+++ lua/modules/ui/init.lua
@@ -1,6 +1,6 @@
 local M = {}
-M.window = require("modules.ui.window")
 M.keymaps = require("modules.ui.keymaps")
 M.config = require("modules.ui.config")
 M.plugins = require("modules.ui.plugins")
+M.statuscolumn = require("modules.ui.statuscolumn")
 return M
