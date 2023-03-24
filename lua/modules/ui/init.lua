0200e8bc525d55b02d8371d568a05124bde2e9a7
Kong Jun <kongjun18@outlook.com>
2023-03-24 14:56:50 +0800

    refactor: move statuscolumn.lua to core

--- lua/modules/ui/init.lua
+++ lua/modules/ui/init.lua
@@ -2,5 +2,4 @@ local M = {}
 M.keymaps = require("modules.ui.keymaps")
 M.config = require("modules.ui.config")
 M.plugins = require("modules.ui.plugins")
-M.statuscolumn = require("modules.ui.statuscolumn")
 return M
