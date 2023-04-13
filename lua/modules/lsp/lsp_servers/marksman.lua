7b52bfae668333048564fab94b8804d51b3ddf4d
Kong Jun <kongjun18@outlook.com>
2023-04-13 16:07:34 +0800

    fix(lsp): marksman.lua formats Go code on save

--- lua/modules/lsp/lsp_servers/marksman.lua
+++ lua/modules/lsp/lsp_servers/marksman.lua
@@ -2,8 +2,8 @@ local M = {}
 
 vim.api.nvim_create_augroup("Markdown", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  desc = "Format Go code on save",
-  pattern = "*.go",
+  desc = "Format markdown file on save",
+  pattern = "*.md",
   callback = function()
     vim.lsp.buf.format()
   end,
