f241140c5b95fef146872972778a60c45eafaad0
Jun Kong <kongjun18@outlook.com>
2023-05-17 21:07:12 +0800

    feat(vcs): add diffview mapping for gv.vim

--- lua/modules/vcs/plugins.lua
+++ lua/modules/vcs/plugins.lua
@@ -12,6 +12,7 @@ local vcs = {
   ["tpope/vim-git"] = {},
   ["junegunn/gv.vim"] = {
     dependencies = "tpope/vim-fugitive",
+    config = config.gv,
   },
   ["akinsho/git-conflict.nvim"] = {
     config = config.git_conflict,
