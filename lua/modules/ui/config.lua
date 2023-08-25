95a6b27cfa5f1adb68e94562583a3142762ef25e
Kong Jun <kongjun18@outlook.com>
2023-08-25 22:20:10 +0800

    chore: upgrade plugins

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -28,7 +28,6 @@ function config.nvim_tree()
       width = 30,
       hide_root_folder = false,
       side = "left",
-      mappings = { custom_only = false, list = {} },
       number = false,
       relativenumber = false,
       signcolumn = "yes",
