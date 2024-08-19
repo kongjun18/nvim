440c25c37d0175dfdd735d1296d8000074ca565e
Jun Kong <kongjun18@outlook.com>
2024-08-19 20:45:55 +0800

    feat(core): support org-mode daily capture

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -2,7 +2,6 @@ local M = {}
 
 M.ft_blacklist = { "qf", "notify", "NvimTree", "Outline"}
 M.bt_blacklist = { "prompt" }
-M.dialy_path = "~/OneDrive/Obsidian/07-笔记/日记.md"
-
+M.dialy_path = path(home, "OneDrive", "Obsidian", "07-笔记", "日记.md")
 
 return M
