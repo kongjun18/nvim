4939cd77aa190d38ad76e39665879cadc2f2c593
Kong Jun <kongjun18@outlook.com>
2025-01-24 17:00:51 +0800

    fix(vcs): gitsigns-yadm.nvim causes errors when exiting git* buffer
    See https://github.com/purarue/gitsigns-yadm.nvim/issues/7 for more
    details. Thanks to @purarue for his help!

--- lua/modules/vcs/config.lua
+++ lua/modules/vcs/config.lua
@@ -3,7 +3,13 @@ local config = {}
 function config.gitsigns()
   local c = {}
   if require("modules.config").yadm_enable then
-    c._on_attach_pre = function(_, callback)
+    c._on_attach_pre = function(buffer, callback)
+      -- Disable gitsigns-yadm.nvim for git* buffer.
+      -- See https://github.com/purarue/gitsigns-yadm.nvim/issues/7.
+      local ft = vim.bo[buffer].filetype
+      if string.match(ft, "git%w+") then
+        return callback()
+      end
       require("gitsigns-yadm").yadm_signs(callback)
     end
   end
