e4911aa98e69a9df625eb93743609ace13072bbb
Jun Kong <kongjun18@outlook.com>
2025-03-14 17:40:57 +0800

    perf(lsp): constrain golangci_lint resource usage

--- lua/modules/lsp/linters/golangci_lint.lua
+++ lua/modules/lsp/linters/golangci_lint.lua
@@ -1,8 +1,10 @@
 local M = {}
 
+vim.env.GOLANGCI_LINT_RUNNER_CPUS = "1"
+
 M.opts = {
   -- Run golangci_lint on a single file to reduce CPU/MEM consuming.
-  extra_args = { "$FILENAME" },
+  extra_args = {"--fast"},
 }
 
 return M
