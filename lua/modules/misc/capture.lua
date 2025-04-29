8567f263b72b85ffb826b4b499e4d0a4e907b68a
Jun Kong <kongjun18@outlook.com>
2025-04-29 11:35:29 +0000

    feat(misc): :Capture supports encrypted diary file

--- lua/modules/misc/capture.lua
+++ lua/modules/misc/capture.lua
@@ -1,44 +1,63 @@
----------------------------------------
 --- Capture Like Org-Mode
 ---------------------------------------
-local function path(...)
-  local path_sep = "/"
-  return table.concat({ ... }, path_sep)
+local ENCRYPTED_PREFIX = "# <<<encrypted>>>"
+
+local function writefile(lines, fname)
+  local success, err = pcall(function()
+    vim.fn.writefile(lines, fname)
+  end)
+
+  if not success then
+    vim.notify(
+      string.format("Failed to write file: %s", err),
+      vim.log.levels.ERROR
+    )
+    return false
+  end
+
+  return true
+end
+
+local function file_exists(file)
+  local f = io.open(file, "rb")
+  if f then
+    f:close()
+  end
+  return f ~= nil
 end
 
 local function insert_list_to(dst, pos, src)
-  -- 检查插入位置是否有效
   if pos > 0 and pos <= #dst + 1 then
-    -- 遍历要插入的列表
     for i = 1, #src do
-      -- 在目标列表的pos位置插入元素
       table.insert(dst, pos, src[i])
-      -- 由于列表插入操作会改变列表长度，需要更新pos位置
       pos = pos + 1
     end
-  else
-    print("插入位置超出目标列表范围")
-    return
   end
 end
 
-local function write_list_to(list, path)
-  local file, err = io.open(path, "w")
-
-  if not file then
-    print("无法打开文件: " .. err)
-    return
-  end
-
-  for _, line in ipairs(list) do
-    file:write(line, "\n") -- \n 表示换行符
-  end
-  file:close()
+local function encrypt_lines(lines, password)
+  local output = vim.fn.systemlist(
+    string.format(
+      "openssl enc -aes-256-cbc -pbkdf2 -salt -in - -out - -k '%s' | base64",
+      password
+    ),
+    lines
+  )
+  return output, vim.v.shell_error == 0
 end
 
--- capture tempfile serves as a lock, which must
--- be deleted after capture buffer exits.
-local function refile_to(buf, capture_path, dst)
+local function decrypt_lines(lines, password)
+  local output = vim.fn.systemlist(
+    string.format(
+      "base64 --decode | openssl enc -d -aes-256-cbc -pbkdf2 -salt -in - -out - -k '%s'",
+      password
+    ),
+    lines
+  )
+  return output, vim.v.shell_error == 0
+end
+
+local function refile_to(buf, dst)
   local date = vim.fn.strftime("%Y-%m-%d %H:%M:%S")
   local matched = {}
   for v in string.gmatch(date, "([^ ]+)") do
@@ -46,13 +65,32 @@ local function refile_to(buf, capture_path, dst)
   end
   local day, time = matched[1], matched[2]
 
+  local password = vim.fn.inputsecret("Enter password: ")
   local lines = {}
-  local lnum = 0
+  for line in io.lines(dst) do
+    table.insert(lines, line)
+  end
+  if #lines == 0 then
+    lines = {}
+  else
+    if lines[1] ~= ENCRYPTED_PREFIX then
+      vim.notify(
+        "Invalid file format: the diary file is not encrypted.",
+        vim.log.levels.ERROR
+      )
+      return
+    end
+    table.remove(lines, 1)
+    lines, ok = decrypt_lines(lines, password)
+    if not ok then
+      vim.notify("Failed to decrypt the diary.", vim.log.levels.ERROR)
+      return
+    end
+  end
+
   local target_heading_lnum = nil
   local previous_heading_lnum = nil
-  for line in io.lines(dst) do
-    lnum = lnum + 1
-    table.insert(lines, line)
+  for lnum, line in ipairs(lines) do
     if not target_heading_lnum then
       local heading = "## " .. day
       if line == heading then
@@ -68,7 +106,7 @@ local function refile_to(buf, capture_path, dst)
   local buf_content_list = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
   table.insert(buf_content_list, 1, time)
   if buf_content_list[#buf_content_list] ~= "\n" then
-    buf_content_list[#buf_content_list + 1] = "\n"
+    buf_content_list[#buf_content_list + 1] = ""
   end
 
   if target_heading_lnum and previous_heading_lnum then
@@ -78,55 +116,41 @@ local function refile_to(buf, capture_path, dst)
     insert_list_to(lines, 1, buf_content_list)
   end
 
-  os.remove(capture_path)
-
-  local file_exists = function(file)
-    local f = io.open(file, "rb")
-    if f then
-      f:close()
-    end
-    return f ~= nil
-  end
-  if not file_exists(dst) then
-    print(string.format("%s not exists", dst))
-    return
-  end
-
-  local tempfile = dst .. ".temp"
-  write_list_to(lines, tempfile)
-  local ok, err = os.rename(tempfile, dst)
+  local encrypted_lines, ok = encrypt_lines(lines, password)
   if not ok then
-    print(string.format("failed to rename %s to %s: %s", tempfile, dst, err))
-    os.remove(tempfile)
+    vim.notify("Failed to encrypt the diary.", vim.log.levels.ERROR)
     return
+  end
+  table.insert(encrypted_lines, 1, ENCRYPTED_PREFIX)
+  if writefile(encrypted_lines, dst) then
+    vim.api.nvim_buf_set_option(buf, "modified", false)
   end
 end
 
 local function capture()
-  -- capture tempfile serves as a lock.
-  local tempfile = path(backup_dir, "capture.md")
-  local stat = vim.loop.fs_stat(tempfile)
-  if stat then
-    print("You are editing the capture buffer in another session!")
-    return
-  end
+  local buf = vim.api.nvim_create_buf(true, false)
+  vim.cmd("sp")
+  local win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(win, buf)
+  vim.api.nvim_buf_set_name(buf, "Capture")
+  vim.api.nvim_buf_set_option(buf, "buftype", "acwrite")
+  vim.api.nvim_buf_set_option(buf, "swapfile", false)
+  vim.api.nvim_buf_set_option(buf, "undofile", false)
+  vim.api.nvim_create_autocmd({ "BufWriteCmd" }, {
+    buffer = buf,
+    callback = function()
+      local diary_path = require("core.config").diary_path
+      if not file_exists(diary_path) then
+        vim.notify(
+          string.format("Diary %s not exists", diary_path),
+          vim.log.levels.ERROR
+        )
+        return
+      end
 
-  local fd = vim.loop.fs_open(tempfile, "w", tonumber("0644", 8))
-  if not fd then
-    print(string.format("failed to open file %s", tempfile))
-    return
-  end
-
-  vim.cmd(string.format("sp %s", tempfile))
+      refile_to(buf, diary_path)
+    end,
+  })
 end
-
-local dialy_path = require("core.config").dialy_path
-vim.api.nvim_create_augroup("Capture", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = { "*/capture.md" },
-  callback = function(ev)
-    refile_to(ev.buf, ev.file, dialy_path)
-  end,
-})
 
 return capture
