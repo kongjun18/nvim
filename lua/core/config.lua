5b32521931629f3dbe646fdda0f19856d83d6c33
kongjun <kongjun18@outlook.com>
2025-04-29 11:35:29 +0000

    feat(misc): :Capture supports encrypted diary file

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -4,6 +4,6 @@ require("core.global")
 M.ft_blacklist = { "qf", "notify", "NvimTree", "Outline" }
 M.bt_blacklist = { "prompt", "nofile" }
 
-M.dialy_path = path(home, "OneDrive", "Obsidian", "06-日记", "日记.md")
+M.diary_path = path(home, "OneDrive", "Obsidian", "06-日记", "日记.md")
 
 return M
