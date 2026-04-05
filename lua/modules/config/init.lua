62eefba7ff2de143089d9576b18d800447cf7bf9
Jun Kong <kongjun18@outlook.com>
2026-04-05 15:32:55 -0700

    feat: add latex support

--- lua/modules/config/init.lua
+++ lua/modules/config/init.lua
@@ -1,9 +1,9 @@
 local M = {}
 
-M.codeium_enable = true
-M.wakatime_enable = true
+M.codeium_enable = false
+M.wakatime_enable = false
 M.yadm_enable = true
-M.obsidian_enable = true
+M.obsidian_enable = false
 M.obsidian_path = "~/Obsidian"
 
 return M
