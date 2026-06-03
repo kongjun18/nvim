e926cd5e5ee0543a968939989725bfe488c8fa8a
Jun Kong <kongjun18@outlook.com>
2026-06-02 21:49:59 -0700

    fix(editor): NvimTree error: system_open has been removed

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -21,7 +21,6 @@ function config.nvim_tree()
       update_cwd = true,
       ignore_list = {},
     },
-    system_open = { cmd = nil, args = {} },
     filters = { dotfiles = false, custom = {} },
     git = { enable = true, ignore = true, timeout = 500 },
     actions = {
